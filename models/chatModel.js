d8bb17f684f3dcc662b880aac5a600fdd0db5004
adi0193 <adiunofficial0193@gmail.com>
2024-06-09 15:52:18 +0530

    innit

---
+++ models/chatModel.js
@@ -0,0 +1,39 @@
+// what this will Contain 
+
+// chatname
+// isgroupchat
+// groupadmin
+// users
+// lastestmessage
+
+
+const mongoose=require("mongoose");
+
+const chatModel=mongoose.Schema({
+    chatName:{type:String,trim:true},
+    isGroupChat:{type:Boolean,default:false},
+    users:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+
+        },
+
+    ],
+    latestMessage:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Message",
+    },
+    groupAdmin:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+},
+{
+  timestamps:true,
+}
+
+);
+const chat=mongoose.model("chat",chatModel)
+
+module.exports=chat;
